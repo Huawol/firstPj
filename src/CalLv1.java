15f014672639b89029f4848a9121052c09fd7f70
taehyeon <max5915@naver.com>
2025-04-16 13:33:24 +0900

    사칙연산 입력 받게 하는거 해결

--- src/CalLv1.java
+++ src/CalLv1.java
@@ -4,6 +4,7 @@ public class CalLv1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             System.out.print("첫번째 숫자를 입력해주세요 : ");
             int num1= scanner.nextInt();
@@ -13,14 +14,17 @@ public class CalLv1 {
             if(num2 == 0){
                 System.out.println("0 을 제외한 숫자를 입력해주세요");
             }
+
             System.out.print("어떤 사칙 연산을 하실건가요? : ");
-            String operations = scanner.nextLine();
+            String operations = scanner.next(); // 반복문 안에서는 next와 nextLine의 차이가 있는데 nextLine에 개행(\n)이 남아서 바로 다음으로 넘어가게됨
             operations.charAt(0);
-            // 연산자 출력
-            switch (operations){
-                case "*":
-                    System.out.println(num1 + " * "+ num2+"="+num1*num2);
+
+            switch (operations.charAt(0)){
+                case '*':
+                    System.out.println(num1 + " * "+ num2+" = "+num1*num2);
                     break;
+                case '+':
+                    System.out.println();
             }
 
 
