52074ecd335c462ef55807630bd15fa15254117b
taehyeon <max5915@naver.com>
2025-04-16 20:29:04 +0900

    내일 와서는 일단 숫자 결과값을 저장 삭제하는 거를 클래스로 옮기는것부터 시작해보자

--- src/calculatorLv2/App.java
+++ src/calculatorLv2/App.java
@@ -1,5 +1,6 @@
 package calculatorLv2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -10,29 +11,44 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int selectNum = 0;
         String exitCommend;
-
+        ArrayList<Object> saveResult = new ArrayList<>();
 
 
         while (true) {
             System.out.println("=============================");
-            System.out.println("1. 계산기              2. 종료 ");
+            System.out.println(" 1. 계산기 2. 종료 3. 숫자 삭제  ");
             System.out.println("=============================");
+            System.out.print("=========== 값을 입력해주세요 : ");
             selectNum = scanner.nextInt();
             if (selectNum == 1) {
+                // 입력란
                 System.out.print("첫번째 정수를 입력하세요 : ");
                 int firstNum = scanner.nextInt();
                 System.out.print("연산자를 입력하세요 : ");
                 String oper = scanner.next();
                 System.out.print("두번째 정수를 입력하세요 : ");
                 int secondNum = scanner.nextInt();
+                // 결과 계산되서 출력디는 란
                 Calculator ca = new Calculator(firstNum,oper,secondNum);
-                System.out.println(ca.result());
+                System.out.println("결과값은 : " + ca.result());
+
+                // 결과값 계속 배열로 축적
+                /*saveResult.add(ca.result())*/;
+
             } else if (selectNum == 2) {
                 System.out.print("종료하기 위해서 exit 을 입력해주세요 : ");
                 exitCommend = scanner.next();
                 if(exitCommend.equals("exit")){
                     System.out.println("종료되었습니다.");
                     return;
+                }
+            } else if (selectNum == 3) {
+                System.out.println("저장 된 숫자: " + saveResult);
+                System.out.print("가장 오래된 값을 지우려면 숫자 0을 입력해주세요 : ");
+                int pick = scanner.nextInt();
+                if(pick == 0) {
+                    saveResult.remove(0);
+                    System.out.println("삭제 되었습니다. 남은 숫자 : " + saveResult);
                 }
             }
         }
--- src/calculatorLv2/Calculator.java
+++ src/calculatorLv2/Calculator.java
@@ -1,10 +1,14 @@
 package calculatorLv2;
 
+import java.util.ArrayList;
+
 public class Calculator {
     // 속성
     int num1;
     int num2;
     char operation;
+    int numResult = 0;
+    /*ArrayList<Object> saveResult = new ArrayList<>();*/
 
     // 생성자 (값 받아오기)
     Calculator(int value1, String value2, int value3) {
@@ -15,8 +19,9 @@ public class Calculator {
     }
 
     // 기능 ( 연산하기 )
+    // 연산 메서든
     int result(){
-        int numResult = 0;
+
         switch (operation){
             case '+' :
                 numResult = num1 + num2;
@@ -39,6 +44,17 @@ public class Calculator {
         }
         return numResult;
     }
+
+    // 결과값 저장 메서드
+    /*int save() {
+        saveResult.add(numResult);
+    }*/
+
+
+
+
+
+
 
     // getter
 
