883ad2ebe02ffce5254f0b8a0aa58a99c41bf9c7
taehyeon <max5915@naver.com>
2025-04-18 11:32:24 +0900

    계산 연산쪽에 재대입으로 인해 변수에 밑줄이 있긴 하지만 멀티 쓰레드에 통해서 값이 변경이 되지는 않을거 같다. (지역변수를 통해서 멀티쓰레드 예방함)

--- src/calculatorLv2/App.java
+++ src/calculatorLv2/App.java
@@ -2,14 +2,14 @@ package calculatorLv2;
 
 import java.util.Scanner;
 
-public class App{
+public class App {
 
     public static void main(String[] args) {
         // 내 생각엔 반복문을 메인 안에 만들어야함
         //
         Scanner scanner = new Scanner(System.in);
-        int selectNum = 0;
-        String exitCommend;
+        /*int selectNum = 0;*/
+        /*String exitCommend;*/
         Calculator ca = new Calculator();
 
         while (true) {
@@ -17,10 +17,11 @@ public class App{
             System.out.println(" 1. 계산기 2. 종료  3. 숫자 삭제 ");
             System.out.println("=============================");
             System.out.print("=========== 값을 입력해주세요 : ");
+            int selectNum = 0;
             selectNum = scanner.nextInt();
 
             switch (selectNum) { // 여기 단계에서 연산자와 두번째 정수를 받은걸로 if문 써서하면 불필요한 코드를 줄일수있다.
-                case 1 :
+                case 1:
                     System.out.print("첫번째 정수를 입력하세요 : ");
                     int firstNum = scanner.nextInt();
 
@@ -40,11 +41,12 @@ public class App{
 
                 case 2:
                     System.out.print("종료하기 위해서 exit 을 입력해주세요 : ");
-                    exitCommend = scanner.next();
-                    if(exitCommend.equals("exit")){
+                    String exitCommend = scanner.next();
+                    if (exitCommend.equals("exit")) {
                         System.out.println("종료되었습니다.");
                         return;
-                    } break;
+                    }
+                    break;
 
                 case 3: // 삭제 하는 기능에는 삭제 기능만 넣기 // 3번 누르면 바로 삭제됨 추가로 0누르는거 없앰
                     System.out.println(ca.saveList);
--- src/calculatorLv2/Calculator.java
+++ src/calculatorLv2/Calculator.java
@@ -5,60 +5,69 @@ import java.util.ArrayList;
 // public 이나  private 사용하기
 public class Calculator {
     // 속성
-    private int firstNum;
+
+    /*private int firstNum;
     private int secondNum;
-    private char operator;
+    private char operator;*/
+    // 계산기 기능에서는 딱히 필요가 없음 멀티 쓰레드 발생시 원하는 결과값이 나오지 않을수 있음
+    // 만약 이용하고 싶으면 메인에 있는 ca를 반복문 안으로 넣고 계산기가 계속 만들어지도록 설정을 해야함
+    //
+
     private int calculatorResult = 0;
     public ArrayList<Integer> saveList = new ArrayList<>();
-    //Object 는 불필요한 공간을 만듬 그러니까 필요한 공간만 사용할수 있도록 타입 설정해주기
+    // 숫자를 저장하기때문에 배열타입을 Interger로만 해도 충분
+    // Object 는 불필요한 공간을 만듬 그러니까 필요한 공간만 사용할수 있도록 타입 설정해주기
     // 리스트에 s 붙이면 복수형이 되니까 리스트 안에 리스트 넣을꺼 아니면 사용하지 않기
 
     // 기본 생성자 꼭필요할때는 명시적으로 작성하는게 좋다.
-    Calculator(){}
+    Calculator() {
+    }
 
     // 기능 ( 연산하기 )
     // 연산 메서든 ( 여기서 값 받아오기 ) 이름을 동사로 바꿔야한다..
     public int calculate(int firstNum, String calculatorOperator, int secondNum) {
-        this.firstNum = firstNum;
+        /*this.firstNum = firstNum;
         this.secondNum = secondNum;
-        this.operator = calculatorOperator.charAt(0);
-        if (this.firstNum < 0) { // 음수를 양수로 전환
-            this.firstNum *= -1;
+        this.operator = calculatorOperator.charAt(0);*/
+        // 주석을 해제해서 사용해도 별문제는 없긴한데 멀티 쓰레드 발생한다는 가정을 하면
+        // 값이 변경되서 출력 될수있음
+
+        if (firstNum < 0) { // 음수를 양수로 전환
+            firstNum *= -1;
         }
         if (secondNum < 0) {
             secondNum *= -1;
         }
 
-        switch (operator){ // 오직 계산기능만 들어감
-            case '+' :
-                calculatorResult = this.firstNum + secondNum;
+        switch (calculatorOperator) { // 오직 계산기능만 들어감
+            case "+":
+                calculatorResult = firstNum + secondNum;
                 return calculatorResult;
-            case '-':
-                calculatorResult = this.firstNum - secondNum;
+            case "-":
+                calculatorResult = firstNum - secondNum;
                 return calculatorResult;
-            case '*':
-                calculatorResult = this.firstNum * secondNum;
+            case "*":
+                calculatorResult = firstNum * secondNum;
                 return calculatorResult;
-            case '/':
-                calculatorResult = this.firstNum / secondNum;
+            case "/":
+                calculatorResult = firstNum / secondNum;
                 return calculatorResult;
 
             default:
-                 System.out.println("잘 못 입력된 값이 있습니다.");
-                 break;
+                System.out.println("잘 못 입력된 값이 있습니다.");
+                break;
         }
         return calculatorResult;
     }
 
     // 결과값 저장 메소드
-    public void saveResult(){
+    public void saveResult() {
         saveList.add(calculatorResult); // 저장 메소드에서 저장하는 기능만 넣어야함
         // 전에 출력문도 같이 넣었다보니 오류가 발생했음
     }
 
     // 결과값 삭제 메소드  0을 굳이 받을 필요는 없다.
     private void removeResult() {
-
         try {
             saveList.remove(0);
             System.out.println("삭제 되었습니다. 남은 숫자 : " + saveList);
